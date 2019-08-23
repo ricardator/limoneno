159025204cc4d9117b8067b0caf451860574a12c
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2019-08-23 03:42:58 -0400

    Modify API configuration

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -2,6 +2,6 @@
  * This its the config environment file
  * The docker replaces the file automatically if the react app deploy in production
  */
-export default class Config {
+export class Config {
     public static API: string = 'http://localhost:3000/api'
 }
