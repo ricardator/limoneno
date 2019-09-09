ad02effe32193544fa3a249034debda278c682e7
alfons0 <aguzman@lemontech.cl>
2019-09-09 17:59:24 -0300

    return update case in back

--- backend/app/api/versions/v1/projects.rb
+++ backend/app/api/versions/v1/projects.rb
@@ -120,38 +120,7 @@ module Versions
       get ':id' do
         id = params[:id]
 
-        project = Project.where(
-          id: id
-        ).includes(:users)
-        .includes(:datasets).first
-
-        tmp = project.attributes
-        tmp[:datasets] = project.datasets
-        tmp[:users] = project.users
-
-        tmp[:assignated] = ProjectDatasetItem.where(
-          project_id: project.id,
-          status: [0, -1]
-        ).count(:id)
-
-        tmp[:assignated_done] = ProjectDatasetItem.where(
-          project_id: project.id,
-          status: 1
-        ).count(:id)
-
-        tmp[:free_pool_done] = ProjectDatasetItem.where(
-          project_id: project.id,
-          status: 2
-        ).count(:id)
-
-        tmp[:free_pool] = Project.where(
-          id: project.id
-        ).joins(:datasets)
-        .joins('INNER JOIN dataset_items ON datasets.id = dataset_items.dataset_id')
-        .joins('LEFT OUTER JOIN project_dataset_items ON project_dataset_items.project_id = projects.id AND dataset_items.id = project_dataset_items.dataset_item_id')
-        .where('project_dataset_items.id IS NULL').count('*')
-
-        tmp
+        project = Project.project_with_dependencies(id)
       end
 
       route_param :project_id do
@@ -184,7 +153,9 @@ module Versions
             end
           end
 
-          status 201
+          status 200
+
+          Project.project_with_dependencies(project_id)
         end
       end
 
--- backend/app/models/project.rb
+++ backend/app/models/project.rb
@@ -1,7 +1,44 @@
+# frozen_string_literal: true
+
 class Project < ApplicationRecord
-    has_many :project_datasets
-    has_many :project_users
-    has_many :project_dataset_items
-    has_many :users, through: :project_users
-    has_many :datasets, through: :project_datasets
+  has_many :project_datasets
+  has_many :project_users
+  has_many :project_dataset_items
+  has_many :users, through: :project_users
+  has_many :datasets, through: :project_datasets
+
+  def self.project_with_dependencies(id)
+    project = Project.where(
+      id: id
+    ).includes(:users)
+    .includes(:datasets).first
+
+    tmp = project.attributes
+    tmp[:datasets] = project.datasets
+    tmp[:users] = project.users
+
+    tmp[:assignated] = ProjectDatasetItem.where(
+      project_id: project.id,
+      status: [0, -1]
+    ).count(:id)
+
+    tmp[:assignated_done] = ProjectDatasetItem.where(
+      project_id: project.id,
+      status: 1
+    ).count(:id)
+
+    tmp[:free_pool_done] = ProjectDatasetItem.where(
+      project_id: project.id,
+      status: 2
+    ).count(:id)
+
+    tmp[:free_pool] = Project.where(
+      id: project.id
+    ).joins(:datasets)
+    .joins('INNER JOIN dataset_items ON datasets.id = dataset_items.dataset_id')
+    .joins('LEFT OUTER JOIN project_dataset_items ON project_dataset_items.project_id = projects.id AND dataset_items.id = project_dataset_items.dataset_item_id')
+    .where('project_dataset_items.id IS NULL').count('*')
+
+    tmp
+  end
 end
