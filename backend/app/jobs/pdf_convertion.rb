ae9a9a05365093a2790602900505fbe69bd6f801
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2019-09-23 16:32:59 -0300

    Integrate midas with limoneno

--- backend/app/jobs/pdf_convertion.rb
+++ backend/app/jobs/pdf_convertion.rb
@@ -1,7 +1,17 @@
+require 'services/midas_service'
 class PdfConvertion < ActiveJob::Base
     queue_as :default
     
-    def perform(file, metadata, dataset_item)
+    def perform(file, dataset)
       # do da ting
+      text = MidasService::MidasClient.get_file_text(file)
+      DatasetItem.update(dataset[:id], {
+        text: text,
+        status: 1
+      });
+    rescue
+      DatasetItem.update(dataset[:id], {
+        status: 3
+      });
     end
 end
