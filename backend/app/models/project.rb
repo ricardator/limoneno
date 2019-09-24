6b9e468201817598875a97218f0b70fc459a405b
Javier Lopez <ameboide@gmail.com>
2019-09-24 19:00:50 -0300

    serializa campos json

--- backend/app/models/project.rb
+++ backend/app/models/project.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Project < ApplicationRecord
+  serialize :entities, JSON
+  serialize :clasifications, JSON
+
   has_many :project_datasets, dependent: :destroy
   has_many :project_users, dependent: :destroy
   has_many :project_dataset_items, dependent: :destroy
--- backend/app/models/project_dataset_item.rb
+++ backend/app/models/project_dataset_item.rb
@@ -1,4 +1,6 @@
 class ProjectDatasetItem < ApplicationRecord
+  serialize :tags, JSON
+
   belongs_to :project
   belongs_to :user
   belongs_to :dataset
