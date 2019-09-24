b1e03bcfef5f466de034813088f0d88fe9d13dc5
Angelo Calvo Alfaro <angelo.calvoa@gmail.com>
2019-09-24 15:41:39 -0300

    Add dataset service tests

--- backend/test/services/dataset_service_test.rb
+++ backend/test/services/dataset_service_test.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
-
-require 'services/midas_service'
 require 'open-uri'
+require "base64"
+require 'database_cleaner'
 
 class DatasetServiceTest < Minitest::Test
+  include FactoryBot::Syntax::Methods
   # Tests for save pdf files
   class PdfDataset < Minitest::Test
     def test_content_or_url_is_nil
@@ -33,16 +34,45 @@ class DatasetServiceTest < Minitest::Test
       end
       assert_equal('The mime type indicates a no pdf file', exception.message)
     end
+
+    def test_upload_pdf
+      DatasetServiceTest.factory
+      uploaded = DatasetService::Files.upload_pdf(mock_data)
+      assert_equal({
+        name: 'sample_file.pdf',
+        dataset_id: 1,
+        mime: 'application/pdf',
+        text: nil,
+        url: 'https://limoneno.s3.amazonaws.com/datasets/1/items/sample_file.pdf',
+        status: 'loading',
+      }, DatasetServiceTest.result(uploaded)[0])
+    end
+
+    def mock_data
+      mock_file_path = "#{Rails.root}/test/fixtures/files/sample_file.pdf"
+      content = File.binread(mock_file_path)
+      encoded = Base64.encode64(content)
+      
+      {
+        name: 'sample_file.pdf',
+        dataset_id: 1,
+        mime: 'application/pdf',
+        data: encoded
+      }
+    end
+
   end
 
   # Tests for save txt files
-  class PdfDataset < Minitest::Test
+  class TxtDataset < Minitest::Test
+    include FactoryBot::Syntax::Methods
+    
     def test_content_or_url_is_nil
 
       mock = {
         name: 'mock.pdf',
         dataset_id: 1,
-        mime: 'plain/text'
+        mime: 'text/plain'
       }
 
       exception = assert_raises Exception do
@@ -56,7 +86,7 @@ class DatasetServiceTest < Minitest::Test
       mock = {
         name: 'mock.pdf',
         dataset_id: 1,
-        mime: 'plain/txt'
+        mime: 'application/json'
       }
 
       exception = assert_raises Exception do
@@ -64,5 +94,88 @@ class DatasetServiceTest < Minitest::Test
       end
       assert_equal('The mime type indicates a no txt file', exception.message)
     end
+
+    def test_upload_txt
+      DatasetServiceTest.factory
+      uploaded = DatasetService::Files.upload_txt(mock_data)
+      assert_equal({
+        name: 'sample_txt.txt',
+        dataset_id: 1,
+        mime: 'text/plain',
+        text: 'Lorem ipsum dolor sit amet',
+        url: 'https://limoneno.s3.amazonaws.com/datasets/1/items/sample_txt.txt',
+        status: 'active'
+      }, DatasetServiceTest.result(uploaded)[0])
+    end
+    
+    def mock_data
+      mock_file_path = "#{Rails.root}/test/fixtures/files/sample_txt.txt"
+      content = File.binread(mock_file_path)
+      encoded = Base64.encode64(content)
+      
+      {
+        name: 'sample_txt.txt',
+        dataset_id: 1,
+        mime: 'text/plain',
+        data: encoded
+      }
+    end
+  end
+
+  # Tests for save txt files
+  class UtilsDataset < Minitest::Test
+    include FactoryBot::Syntax::Methods
+
+    def test_save_dataset
+      DatasetServiceTest.factory
+      dataset = DatasetService::Files.save_dataset([{
+        name: 'sample_txt.txt',
+        dataset_id: 1,
+        mime: 'text/plain',
+        text: 'Lorem ipsum dolor sit amet',
+        url: 'https://limoneno.s3.amazonaws.com/datasets/1/items/sample_txt.txt',
+        status: DatasetItem.statuses[:active]
+      }])
+      assert_equal([{
+        name: 'sample_txt.txt',
+        dataset_id: 1,
+        mime: 'text/plain',
+        text: 'Lorem ipsum dolor sit amet',
+        url: 'https://limoneno.s3.amazonaws.com/datasets/1/items/sample_txt.txt',
+        status: 'active'
+      }],DatasetServiceTest.result(dataset))
+    end
+  end
+
+  def self.factory
+    test = new DatasetServiceTest
+    test.clean_database
+    test.create_dataset
+  end
+
+  def self.result(data)
+    result = []
+
+    data.each do |item|
+      result.push({
+        name: item[:name],
+        dataset_id: item[:dataset_id],
+        mime: item[:mime],
+        text: item[:text],
+        url: item[:url],
+        status: item[:status]
+      })
+    end
+
+    result
+  end
+
+  def create_dataset
+    create(:dataset)
+  end
+
+  def clean_database
+    DatabaseCleaner.strategy = :truncation, {:except => %w[	ar_internal_metadata]}
+    DatabaseCleaner.clean
   end
 end
