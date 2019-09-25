11f244db8c7da28f62370bae2aae2ccfe338b671
alfons0 <aguzman@lemontech.cl>
2019-09-25 13:11:16 -0300

    more free_pool tests

--- backend/test/factories/project_datasets.rb
+++ backend/test/factories/project_datasets.rb
@@ -3,11 +3,20 @@
 FactoryBot.define do
   factory :project_dataset do
     initialize_with do
-      item = create(:dataset_item)
       new(
-        dataset_id: item.dataset_id,
+        dataset_id: create(:dataset).id,
         project_id: create(:project).id
       )
+    end
+
+    trait :with_item do
+      initialize_with do
+        item = create(:dataset_item)
+        new(
+          dataset_id: item.dataset_id,
+          project_id: create(:project).id
+        )
+      end
     end
   end
 end
--- backend/test/models/project_test.rb
+++ backend/test/models/project_test.rb
@@ -3,11 +3,19 @@ require 'test_helper'
 class ProjectTest < Minitest::Test
   include FactoryBot::Syntax::Methods
 
-  def test_free_pool
+  def test_free_pool_with_dataset_items
+    project_dataset = create(:project_dataset, :with_item)
+
+    free_pool_size = Project.free_pool(project_dataset.project_id).size
+
+    assert_equal(1, free_pool_size)
+  end
+
+  def test_free_pool_without_dataset_items
     project_dataset = create(:project_dataset)
 
-    items = Project.free_pool(project_dataset.project_id).size
+    free_pool_size = Project.free_pool(project_dataset.project_id).size
 
-    assert_equal(1, items)
+    assert_equal(0, free_pool_size)
   end
 end
