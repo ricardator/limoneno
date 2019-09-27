a3e908ccb82bb9e6426e2f94db5b1b4397a4d783
alfons0 <aguzman@lemontech.cl>
2019-09-27 12:16:30 -0300

    fix: migrations

--- backend/db/migrate/20190924191051_update_project_dataset_items.rb
+++ backend/db/migrate/20190924191051_update_project_dataset_items.rb
@@ -1,6 +1,5 @@
 class UpdateProjectDatasetItems < ActiveRecord::Migration[6.0]
   def change
     change_column :project_dataset_items, :clasification, :text, size: :long
-    change_column :project_dataset_items, :documents, :boolean
   end
 end
