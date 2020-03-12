0eb28ee1f791a98050ec632154ce65367863853d
Giorgio Giannoni <ggiannoni@lemontech.com>
2020-03-12 12:08:15 -0300

    fix: bug that prevented item assignations to users

--- backend/app/models/project_dataset_item.rb
+++ backend/app/models/project_dataset_item.rb
@@ -1,8 +1,16 @@
+# frozen_string_literal: true
+
 class ProjectDatasetItem < ApplicationRecord
   belongs_to :project
   belongs_to :user
   belongs_to :dataset
   belongs_to :dataset_item
+
+  # Statuts meaning
+  # 0  Asignado pero no revisado
+  # 1  asignados
+  # 2  auto asignados
+  # -1  multiple actuacion
 
   def self.create_users_pool(users_pool, project_id)
     free_pool = Project.find(project_id).free_pool.pluck(:id, :dataset_id)
@@ -13,8 +21,8 @@ class ProjectDatasetItem < ApplicationRecord
           user_id: user_id,
           project_id: project_id,
           status: 0,
-          dataset_id: item.dataset,
-          dataset_item_id: item.id
+          dataset_id: item[1],
+          dataset_item_id: item[0]
         )
       end
     end
